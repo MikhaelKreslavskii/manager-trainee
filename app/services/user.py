97c533d95901dc34abbc3de8f69655a42993d7d5
Mikhael Kreslavskii <mikhaelkreslavskii@gmail.com>
2025-02-07 02:15:20 +0300

    Переименовал функцию

--- app/services/user.py
+++ app/services/user.py
@@ -10,7 +10,7 @@ from bson import ObjectId
 
 # from app.schemas.user import UserInDB
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/users/login")
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
