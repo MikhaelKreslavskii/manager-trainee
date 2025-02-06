5c27a772e22745c05e6e3f1f24ccb35565b402d9
Mikhael Kreslavskii <mikhaelkreslavskii@gmail.com>
2025-02-07 02:24:31 +0300

    Убрал лишний инит secret ket

--- app/services/user.py
+++ app/services/user.py
@@ -1,4 +1,7 @@
+import os
+
 import jwt
+from dotenv import load_dotenv
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
@@ -8,13 +11,12 @@ from app.schemas import User
 from typing import Union
 from bson import ObjectId
 
+from app.services.auth import SECRET_KEY, ALGORITHM
+
 # from app.schemas.user import UserInDB
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/users/login")
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class UserService:
